65d547a7fafafefbee8ed5d2de6ff8d2255a6d3a
francesco <francesco.capalbo107@gmail.com>
2026-01-19 14:34:57 +0100

    funzionamento main.php

--- main.php
+++ main.php
@@ -1,49 +1,9 @@
-
 <?php
 
-require_once 'Wagon.php';
-require_once 'Train.php';
+declare(strict_types=1);
 
-/* ====== WAGONS ====== */
+require __DIR__ . '/models/Wagon.php';
+require __DIR__ . '/models/Train.php';
 
 $wagon1 = new Wagon(40);
-$wagon2 = new Wagon(40);
-$wagon3 = new Wagon(40);
-
-echo $wagon1->passengers_count() . PHP_EOL; // 0
-echo $wagon1->seats_count() . PHP_EOL;      // 40
-
-echo $wagon1->add_passengers(10) . PHP_EOL; // 0
-echo $wagon1->passengers_count() . PHP_EOL; // 10
-
-echo $wagon1->add_passengers(55) . PHP_EOL; // 25
-echo $wagon1->passengers_count() . PHP_EOL; // 40
-
-$wagon1->remove_passengers(15);
-echo $wagon1->passengers_count() . PHP_EOL; // 25
-
-$wagon1->remove_passengers(25);
-echo $wagon1->passengers_count() . PHP_EOL; // 0
-
-
-/* ====== TRAIN ====== */
-
-$train = new Train();
-$train->add_wagon($wagon1);
-$train->add_wagon($wagon2);
-$train->add_wagon($wagon3);
-
-echo $train->passengers_count() . PHP_EOL;  // 0
-echo $train->seats_count() . PHP_EOL;       // 120
-
-echo $train->add_passengers(10) . PHP_EOL;  // 0
-echo $train->passengers_count() . PHP_EOL;  // 10
-print_r($train->passengers_distribution()); // [10, 0, 0]
-
-echo $train->add_passengers(100) . PHP_EOL; // 0
-echo $train->passengers_count() . PHP_EOL;  // 110
-print_r($train->passengers_distribution()); // [40, 40, 30]
-
-$train->remove_passengers(35);
-echo $train->passengers_count() . PHP_EOL;  // 75
-print_r($train->passengers_distribution()); // [40, 35, 0]
+echo $wagon1->passangers_count();
--- models/wagon.php
+++ models/wagon.php
@@ -1,31 +1,40 @@
 <?php
 
-class Wagon {
-    private int $seats;
-    private int $passengers = 0;
+declare(strict_types=1);
 
-    public function __construct(int $seats) {
-        $this->seats = $seats;
+final class Wagon
+{
+    private int $seats; //posti totali
+    private int $passangers = 0; //posti dei passeggeri nel vagone 
+    
+
+    public function __construct(private int $seat)
+    {
+        $this->seats = $seat;
     }
 
-    public function seats_count(): int {
+    public function passangers_count()
+    {
+        return $this->passangers;
+    }
+
+    public function seats_count()
+    {
         return $this->seats;
     }
 
-    public function passengers_count(): int {
-        return $this->passengers;
+    public function add_passangers(int $num)
+    {
+        $free_seats = $this->seats - $this->passangers;
+        if($num >= $free_seats){
+            
+        }
     }
 
-    public function add_passengers(int $count): int {
-        $available = $this->seats - $this->passengers;
-        $to_add = min($available, $count);
-        $this->passengers += $to_add;
-        return $count - $to_add; // passeggeri in eccesso
-    }
-
-    public function remove_passengers(int $count): int {
-        $to_remove = min($this->passengers, $count);
-        $this->passengers -= $to_remove;
-        return $count - $to_remove; // passeggeri che non si è potuto rimuovere
-    }
 }
+
+
+
+
+
+
